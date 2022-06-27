de8600f35a6f0b7b4896bad30746ae257539260f
Hughie <acubu6969@gmail.com>
2022-06-27 21:55:18 +0700

    re

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Home} from './Home';
+//import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1 className="d-flex justify-content-center m-3">
+      Vought Supes Manager
+      </h1>
+     
+      <Home/>
+      
     </div>
   );
 }
---
+++ src/variables.js
@@ -0,0 +1,3 @@
+export const variables= {
+    API_URL: "https://localhost:7087/api/Supes"
+}
